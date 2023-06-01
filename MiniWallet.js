c70916c62d294ee90863da9e026975ec061c2651
Aniket Bansode <aniket.bansode2311@gmail.com>
2023-06-01 16:34:33 +0530

    initial commit

---
+++ MiniWallet.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const Web3 = require('web3');
+const apikey = process.env['apikey']
+const network = 'sepolia';
+
+const node = `https://eth.getblock.io/${apikey}/${network}/`
+const web3 = new Web3(node);
+
+//consoole.log(web3)
+
+const accountTo = web3.eth.accounts.create();
+// console.log(accountTo);
+// console.log(accountTo.address);
+
+const privateKey = process.env['privateKey'];
+const accountFrom = web3.eth.accounts.privateKeyToAccount(privateKey);
+// console.log(accountFrom);
+
+const createSignedTx = async(rawTx) => {
+    rawTx.gas = await web3.eth.estimateGas(rawTx);
+    return await accountFrom.signTransaction(rawTx);
+}
+
+const sendSignedTx = async(signedTx) => {
+    web3.eth.sendSignedTransaction(signedTx.rawTransaction).then(console.log)
+}
+
+const amountTo = "0.01"    //ether
+const rawTx = {
+    to:accountTo.address,
+    value:web3.utils.toWei(amountTo, "ether")
+}
+createSignedTx(rawTx).then(sendSignedTx)
